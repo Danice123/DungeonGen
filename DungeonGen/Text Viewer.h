61512f2b0108fd038c521e7a1e41f5a382e2db45
curtismj1 <curtismj1@gcc.edu>
2014-12-03 20:32:59 -0500

    Smooth text scrolling

--- DungeonGen/Text Viewer.h
+++ DungeonGen/Text Viewer.h
@@ -1,6 +1,8 @@
 #include <iostream>;
 #include <Windows.h>
 #include "DungeonGen.h"
+#include <process.h>
+using namespace std;
 class TextViewer{
 public:
 	TextViewer(){width = 0; height = 0; x_pos = 0; y_pos = 0; is_drawing = true;}
@@ -14,50 +16,67 @@ private:
 
 
 
+void gotoxy(int x, int y) 
+{ 
+  HANDLE hConsoleOutput; 
+  COORD dwCursorPosition;
+
+  cout.flush(); 
+  dwCursorPosition.X = x; 
+  dwCursorPosition.Y = y; 
+  hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE); 
+  SetConsoleCursorPosition(hConsoleOutput,dwCursorPosition); 
+}
+
 void TextViewer::draw(DungeonGen gen){
 
+	const int WIDTH = gen.getFloor(0).getWidth();
+	const int HEIGHT = gen.getFloor(0).getHeight();
+	system("cls");
+	gotoxy(0,0);
 
-	for(int a = x_pos; a < x_pos+40 && a < gen.getFloor(0).getWidth(); a++){
-		for (int b = y_pos; b<y_pos+70 && b< gen.getFloor(0).getHeight(); b++){
-					std::cout << gen.getFloor(0).getTile(a, b);
+			for(int a = y_pos; a < y_pos+25 && a < HEIGHT; a++){
+				for (int b = x_pos; b<x_pos+75 && b< WIDTH; b++){
+					std::cout << gen.getFloor(0).getTile(b, a);
 				}
-				std::cout << std::endl;
-			}
+				cout << endl;
+			}	
 	while(true){
+		gotoxy(0,0);
 		while(true){
 			if(GetAsyncKeyState(VK_RIGHT)){
-				y_pos++;
-				Sleep(10);
+				x_pos++;
+				//Sleep(10);
 				break;
 			}
 			else if(GetAsyncKeyState(VK_LEFT)){
-				y_pos--;
-				Sleep(10);
+				x_pos--;
+				//Sleep(10);
 				break;
 			}
 			else if(GetAsyncKeyState(VK_UP)){
-				x_pos--;
-				Sleep(10);
+				y_pos--;
+				//Sleep(10);
 				break;
 			}
 			else if(GetAsyncKeyState(VK_DOWN)){
-				x_pos++;
-				Sleep(10);
+				y_pos++;
+				//Sleep(10);
 				break;
 			}
 		}
-		system("cls");
+		gotoxy(0,0);
 		if(x_pos < 0){
 			x_pos = 0;
 		}
 		if(y_pos < 0){
 			y_pos = 0;
 		}
-			for(int a = x_pos; a < x_pos+40 && a < gen.getFloor(0).getWidth(); a++){
-				for (int b = y_pos; b<y_pos+70 && b< gen.getFloor(0).getHeight(); b++){
-					std::cout << gen.getFloor(0).getTile(a, b);
+			for(int a = y_pos; a < y_pos+25 && a < HEIGHT; a++){
+				for (int b = x_pos; b<x_pos+75 && b< WIDTH; b++){
+					std::cout << gen.getFloor(0).getTile(b, a);
 				}
-				std::cout << std::endl;
+				cout << endl;
 			}	
 	}
 }
